e73cf45b8bc44af8eaef76e7a1432b10f63bf996
Sergey Bolshakov <Illidan.Ice@gmail.com>
2023-12-01 16:50:36 +0300

    PeredelkaMetly

--- Kursovaya2/Gonka/Gonka/Broom.cpp
+++ Kursovaya2/Gonka/Gonka/Broom.cpp
@@ -8,9 +8,13 @@ Broom::Broom() {
 }
 
 double Broom::time_count(int trassa) {
-	double hours = 0, track = 0, crs = cruise ;
+	double hours = 0, track = 0, crs = cruise;
+	int c = 0;
+	c = trassa / 1000;
+	crs = c;
+	trassa *= ((100 - crs) / 100);
 	for (int i = 0; i < 2;) {
-		for (double j = 0; j < (10 * (100 - crs)) / speed ; ++j) {
+		for (double j = 0; j < 50; ++j) {
 			if ((static_cast<double>(trassa) - track) < speed) {
 				if (static_cast<double>(trassa) > track) {
 					hours += (static_cast<double>(trassa) - track) / speed;
@@ -21,16 +25,13 @@ double Broom::time_count(int trassa) {
 			else {				
 				track += speed;
 				++hours;
-			}
-			if (10 * (100 - crs) / speed - j == 0.5) hours += 0.5;
+			}			
 			if (track >= trassa) {
 				i = 2; break;
 			}
 		}		
-		if (i == 2) break;		
-		crs += 1.0;
-		trassa -= 10 * crs;
-		if (trassa <= 0) break; // если пройденная дистанция > 100000 - исходя из переданных по условию задачи параметров, начинаются парадоксы. По итогам, приходиться делать так, что после этого значения, метла просто телепортируется к финишу
+		if (i == 2) break;				
+		if (trassa <= 0) break; 
 	}
 	return hours;
 }
